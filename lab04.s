2e9f438fd62b4221bc1751ff5935a8c24032b19d
charitou <cs05556@uoi.gr>
2024-11-12 21:28:48 +0200

    Completed lab04

--- lab04.s
+++ lab04.s
@@ -26,11 +26,18 @@ arrayNotSorted: .word marianna, markos, maria
             ecall
 
 str_ge:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
-            jr   ra
+    lbu t0,0(a0)
+    lbu t1, 0(a1)
+    sub t2, t0,t1
+    addi a0,a0,1
+    addi a1,a1,1
+    add t3,t1,t0
+    beq t3,t0,ret_str
+    beq t2,zero,str_ge
+ret_str:
+    srli a0,t2,31
+    xori a0,a0,1
+    jr   ra
  
 # ----------------------------------------------------------------------------
 # recCheck(array, size)
@@ -42,8 +49,25 @@ str_ge:
 #     return 0
 
 recCheck:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
-            jr   ra
+    slti t0,a1,2
+    beq t0,zero,checkFT
+    addi a0,zero,1
+    jr   ra
+checkFT:
+    addi sp,sp, -12
+    sw ra,8(sp)
+    sw a0,4(sp)
+    sw a1, 0(sp)
+    lw a1,0(a0)
+    lw a0, 4(a0)
+    jal str_ge
+    beq a0,zero,return
+    lw a0,4(sp)
+    lw a1,0(sp)
+    addi a0,a0,4
+    addi a1,a1,-1
+    jal recCheck
+return:
+    lw ra,8(sp)
+    addi sp,sp,12
+    jr ra
